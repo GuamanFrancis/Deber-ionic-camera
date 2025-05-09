d6efb4e948289d825962534b3647056c2d035505
GuamanFrancis <francisguaman909@gmail.com>
2025-05-08 22:46:55 -0500

    Modificando los archivos para el ejercicio 3

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit} from '@angular/core';
 import { PhotoService,UserPhoto } from '../services/photo.service';
-
+//ejercicio 3 para mostar las fotos
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -13,7 +13,8 @@ export class Tab1Page {
   constructor(public photoService:PhotoService) {}
 
   
-
+// funcion mostrar fotos
+  // funcion para mostrar las fotos
   async mostrarFotos() {
     await this.photoService.loadSaved();
     this.photos = this.photoService.photos;
